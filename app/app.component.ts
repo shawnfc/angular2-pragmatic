37f6f5feafd5a774aac672ab057a29e2cc064653
shawnfc <shawn@shawnconrad.com>
2016-08-30 15:13:38 -0400

    moving to RC5 version

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,31 +1,14 @@
-import {Component} from 'angular2/core';
-import {TweetComponent} from './tweet.component'
-import {BootstrapPanel} from './bootstrap.panel.component'
-import {ZippyComponent} from './Zippy.component'
+import {Component} from 'angular2/core'
+import {ContactFormComponent} from './contact-form.component'
 
 @Component({
     selector: 'my-app',
     template: `
-        <ul class="nav nav-pills">
-            <li [class.active]="viewMode == 'map'"><a (click)="viewMode = 'map'">Map View</a></li>
-            <li [class.active]="viewMode == 'list'"><a (click)="viewMode = 'list'">List View</a></li>
-        </ul>
-        <div [ngSwitch]="viewMode">
-        <template [ngSwitchWhen]="'map'" ngSwitchDefault>Map View Content</template>
-        <template [ngSwitchWhen]="'list'">List View Content</template>
-        
-        <zippy>
-            <div class="zipheading1"></div>
-            <div class="zipbody1">The body for Zip 1</div>
-            <div class="zipheading2">Zip Heading 2</div>
-            <div class="zipbody2">The body for Zip 2</div>
-        </zippy>
-</div>
+        <contact-form></contact-form>
     `,
-    directives: [ZippyComponent]
+    directives: [ContactFormComponent]
 
 })
 
 export class AppComponent {
-    viewMode = 'map';
 }
