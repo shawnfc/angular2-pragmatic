d42d2d9307a9231ee16d71259e6ecdad258ea9a7
shawnfc <shawn@shawnconrad.com>
2016-10-01 09:11:14 -0400

    create starter of final release

--- app/app.component.js
+++ app/app.component.js
@@ -16,7 +16,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>My SECOND Angular App</h1>'
+            template: '<h1>My Angular App</h1>'
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
